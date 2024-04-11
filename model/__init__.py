e2aa2a84706f9322b75d8dbe6161868773f88cba
sowmi234 <137216637+sowmi234@users.noreply.github.com>
2024-04-11 22:45:58 +0530

    Add files via upload

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+from model.keypoint_classifier.keypoint_classifier import KeyPointClassifier
+from model.point_history_classifier.point_history_classifier import PointHistoryClassifier
+
+
+
+point_history_classifier = PointHistoryClassifier()
---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+from utils.cvfpscalc import CvFpsCalc
+
+# Create an instance of CvFpsCalc with default buffer length
+cvFpsCalc = CvFpsCalc()
+
+
